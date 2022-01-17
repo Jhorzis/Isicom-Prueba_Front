b3d94d9824148db3afcbfa7c0e1ea2c681bc1fed
Andy J <mrcanhales66@gmail.com>
2022-01-17 11:41:53 -0500

    Primer commit real

---
+++ src/app/ProductoGenerico/ProductoGenerico.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'ProductoGenerico',
+    templateUrl: './ProductoGenerico.Component.html'
+})
+export class ProductoGenericoComponent{
+    
+}
