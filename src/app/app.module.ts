7e79efe48f650f7a92ced84f2dc911bfdd3628ff
Andy J <mrcanhales66@gmail.com>
2022-01-20 18:48:26 -0500

    ll

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { routing, appRoutingProviders } from './app.routing';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductoGenericoComponent } from './ProductoGenerico/ProductoGenerico.component';
+import { ProdGenComponent } from './prod-gen/prod-gen.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductoGenericoComponent
+    ProductoGenericoComponent,
+    ProdGenComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/prod-gen/prod-gen.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-prod-gen',
+  templateUrl: './prod-gen.component.html',
+  styleUrls: ['./prod-gen.component.css']
+})
+export class ProdGenComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
